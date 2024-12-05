57f6365336ad208063f5e8067451e320efeab94c
zah-svg <hanasy171@gmail.com>
2024-12-05 20:53:36 +0700

    Add files via upload

---
+++ Pembayaran.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+int main(){
+    int pilihan;
+    do{
+    printf("==========PEMBAYARAN============\n");
+    printf("KETIK 1 : BANK BRI\n");
+    printf("KETIK 2 : BANK MANDIRI\n");
+    printf("================================\n");
+    printf("MASUKKAN PILIHAN ANDA : ");
+    scanf("%d", &pilihan);
+
+    switch(pilihan){
+        case 1 : rekeningbri();
+        break;
+        case 2 : rekeningmandiri();
+        break;
+        default : printf("ANGKA YANG ANDA MASUKKAN TIDAK VALID\n");
+        break;
+    }
+    }while(pilihan != 2);
+    return 0;
+}
+
+void rekeningbri(){
+    char RekeningInput[50];
+    char RekeningFile[50];
+    int Saldo;
+    int found = 0;
+
+    printf("MASUKKAN NOMOR REKENING ANDA : ");
+    scanf("%s", RekeningInput);
+
+    FILE *rekening = fopen("rekeningbri.txt", "r");
+    if(rekening == NULL){
+        printf("REKENING ANDA TIDAK DITEMUKAN.\n");
+        return;
+    }
+
+    while(fscanf(rekening, "%s %d", RekeningFile, &Saldo) != EOF){
+        if(strcmp(RekeningInput, RekeningFile) == 0){
+            printf("Saldo Anda : Rp %d\n", Saldo);
+            found = 1;
+            break;
+        }
+    }
+    fclose(rekening);
+    if(!found){
+        printf("REKENING ANDA TIDAK DITEMUKAN!\n");
+    }   
+}
+
+void rekeningmandiri(){
+    char RekeningInput[50];
+    char RekeningFile[50];
+    int Saldo;
+    int found = 0;
+
+    printf("MASUKKAN NOMOR REKENING ANDA : ");
+    scanf("%s", RekeningInput);
+
+    FILE *rekening = fopen("rekeningmandiri.txt", "r");
+    if(rekening == NULL){
+        printf("REKENING ANDA TIDAK DITEMUKAN.\n");
+        return;
+    }
+
+    while(fscanf(rekening, "%s %d", RekeningFile, &Saldo) != EOF){
+        if(strcmp(RekeningInput, RekeningFile) == 0){
+            printf("Saldo Anda : Rp %d\n", Saldo);
+            found = 1;
+            break;
+        }
+    }
+    fclose(rekening);
+    if(!found){
+        printf("REKENING ANDA TIDAK DITEMUKAN!\n");
+    }   
+}
